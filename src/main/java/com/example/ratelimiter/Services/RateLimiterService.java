fd62422785ea4d0a6391264b33e85e1542eec0de
Siuujal_3 <sujalbkkashid@gmail.com>
2026-06-12 17:25:28 +0530

    Service method completed for handling requests

--- src/main/java/com/example/ratelimiter/Services/RateLimiterService.java
+++ src/main/java/com/example/ratelimiter/Services/RateLimiterService.java
@@ -1,21 +1,39 @@
 package com.example.ratelimiter.Services;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import com.example.ratelimiter.Models.UserRequestInfo;
 
 @Service
 public class RateLimiterService 
 {
+
     Map<String, UserRequestInfo> requests;
+
+    @Value("${app.request.limit}")
+    private int limit;
+
+    @Value("${app.request.window}")
+    private Duration window;
 
     public RateLimiterService()
     {
         this.requests = new ConcurrentHashMap<>();
     }
 
-    
+    public boolean registerRequest(String ipAddress)
+    {
+        UserRequestInfo userRequestInfo = requests.computeIfAbsent(ipAddress, key -> new UserRequestInfo());
+
+        Instant currentTime = Instant.now();
+
+        return userRequestInfo.tryAndRequest(currentTime, limit, window);
+    }
+
 }
