a3f8b6e239bac379fa6b4b4366e5e9d76676d123
Siuujal_3 <sujalbkkashid@gmail.com>
2026-06-13 15:39:37 +0530

    Registered the RateLimiterInterceptor

---
+++ src/main/java/com/example/ratelimiter/Configurations/RateLimiterInterceptorConfig.java
@@ -0,0 +1,21 @@
+package com.example.ratelimiter.Configurations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.example.ratelimiter.Interceptors.RateLimiterInterceptor;
+
+@Configuration
+public class RateLimiterInterceptorConfig implements WebMvcConfigurer
+{
+    @Autowired
+    private RateLimiterInterceptor rateLimiterReceptor;
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry interceptorRegistry)
+    {
+        interceptorRegistry.addInterceptor(rateLimiterReceptor).addPathPatterns("/**");
+    }
+}
